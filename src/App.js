2ce6e97f52609b6bd04381c2473810854acf3320
Robin Joseph <48469274+rj-robinjoseph@users.noreply.github.com>
2021-07-19 00:22:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import menu from "../src/images/Menu.svg"
+import logo from "../src/images/Logo.png"
+import search from "../src/images/Search.svg"
+import image from "../src/images/Car.png"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+          <img src={menu} className="menu" alt="" />
+          <img src={logo} className="logo" alt="" />
+          <img src={search} className="search" alt="" />
+      </div>
+      <div className="container">
+        <h1 className="heading">The All New <span className="big-h1">S-Class</span></h1>
+      </div>
+      <div className="container">
+        <img src={image} className="image" alt="" />
+      </div>
+      <div className="w-layout-grid grid">
+        <div className="feature-box">
+          <h1 className="feature-heading">Luxury</h1>
+          <div className="text-block block-1">The greatest refinement out there paired with technological brilliance.</div>
+        </div>
+        <div className="feature-box">
+          <h1 className="feature-heading">362 hp</h1>
+          <div className="text-block block-2">The S-class has plenty of punch when driven at full tilt on the Autobahn.</div>
+        </div>
+        <div className="feature-box last-box">
+          <h1 className="feature-heading">Twin turbo</h1>
+          <div className="text-block block-3">Simultaneous performance and luxury. A unique unmatched synergy.</div>
+        </div>
+      </div>
+
     </div>
   );
 }
