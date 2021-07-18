f7c297363e3dbe9623af60910156ae95c76dc85b
Robin Joseph <48469274+rj-robinjoseph@users.noreply.github.com>
2021-07-19 00:39:13 +0530

    Animations Added

--- src/App.js
+++ src/App.js
@@ -17,18 +17,18 @@ function App() {
          <h1 className="heading">The All New <span className="big-h1">S-Class</span></h1>
       </div>
       <div className="container">
-         <img src={image} className="image" alt="" />
+         <img src={image} className="image animate fadeInUp" alt="" />
       </div>
       <div className="w-layout-grid grid">
-        <div className="feature-box">
+        <div className="feature-box box-1 animate two fadeInUp">
            <h1 className="feature-heading">Luxury</h1>
            <div className="text-block block-1">The greatest refinement out there paired with technological brilliance.</div>
         </div>
-        <div className="feature-box">
+        <div className="feature-box box-2 animate three fadeInUp">
            <h1 className="feature-heading">362 hp</h1>
            <div className="text-block block-2">The S-class has plenty of punch when driven at full tilt on the Autobahn.</div>
         </div>
-        <div className="feature-box last-box">
+        <div className="feature-box box-3 animate four fadeInUp">
            <h1 className="feature-heading">Twin turbo</h1>
            <div className="text-block block-3">Simultaneous performance and luxury. A unique unmatched synergy.</div>
         </div>
